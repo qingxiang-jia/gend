7ce8de956acca08892504e62f5367e7f41aeb08a
Qingxiang Jia <ybjqx3340@gmail.com>
2018-09-16 17:19:27 -0700

    Allow CORS

--- gend.go
+++ gend.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/gorilla/handlers"
 )
 
 func main() {
+	allowedHeaders := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"})
+
 	handler := http.FileServer(http.Dir("C:/Users/ybjqx/Music/"))
 	http.Handle("/", http.StripPrefix("/", handler))
-	http.ListenAndServe(":8081", nil)
+	http.ListenAndServe(":8081", handlers.CORS(allowedHeaders, allowedOrigins, allowedMethods)(handler))
 }
